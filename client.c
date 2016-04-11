a642f6d888d717637ce98dce0ae6fc0c32374bf9
Casey Carroll <aiyakc@gmail.com>
2016-04-11 14:10:47 -0500

    added TCP structure and added connect_to_server()

--- client.c
+++ client.c
@@ -5,9 +5,27 @@
 #include <arpa/inet.h>
 #include <string.h>
 
+
+//initialize TCP header struct
+struct tcp_hdr
+{
+    short int src;
+    short int des;
+    int seq;
+    int ack;
+    short int hdr_flags;
+    short int rec;
+    short int cksum;
+    short int ptr;
+    int opt;
+};
+
+int connect_to_server(int portno);
+
 int main(int argc, char **argv)
 {
-	int portno, sockfd;
+	int portno;
+	int num_attempts = 0;
 	struct sockaddr_in serv_addr;
 	struct hostnet *server;
 
@@ -18,26 +36,49 @@ int main(int argc, char **argv)
 		exit(0);
 	}
 
-   	//set port number from user input
-   	portno = atoi(argv[1]);
+	//set port number from user input
+	portno = atoi(argv[1]);	
 
+	//attempts to connect to server three times
+	while(num_attempts < 2)
+	{
+		if(connect_to_server(portno) == 0)
+		{
+			//successful connection, break out of loop
+			num_attempts = 2;
+		} else {
+			printf("attempting to connect again...\n");
+		}
+
+		num_attempts++;
+	}
+
+	return 0;
+}
+
+int connect_to_server(int portno)
+{
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	struct hostnet *server;
+   	
    	//create socket
    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
    	if(sockfd < 0 )
    	{
    		printf("Error creating socket\n");
-   		exit(1);
+   		return(-1);
    	}
 
    	serv_addr.sin_family = AF_INET;
-      serv_addr.sin_addr.s_addr=inet_addr("129.120.151.94"); //IP for cse01.cse.unt.edu
+  	serv_addr.sin_addr.s_addr=inet_addr("129.120.151.94"); //IP for cse01.cse.unt.edu
    	serv_addr.sin_port = htons(portno);
 
    	/* Connecting to the server */
 	if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
 		printf("ERROR while attempting to  connect");
-		exit(1);
+		return(-1);
 	}
 
-	return 0;
+	return 0;//success
 }
