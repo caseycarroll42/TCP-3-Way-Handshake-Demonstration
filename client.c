2fd1678cfe4daf3559d17fa4f6e3d92b09dadbf7
Casey Carroll <aiyakc@gmail.com>
2016-04-11 20:27:17 -0500

    add in checksum code, memcpy struct to char array and send to server

--- client.c
+++ client.c
@@ -4,6 +4,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <sys/socket.h>
 
 
 //initialize TCP header struct
@@ -22,10 +23,15 @@ struct tcp_hdr
 
 int main(int argc, char **argv)
 {
-	int portno, sockfd;
+	int portno, sockfd, local_addr_len, num_sent;
 	int num_attempts = 0;
 	struct sockaddr_in serv_addr;
+  struct sockaddr_in local_addr;
 	struct hostnet *server;
+  struct tcp_hdr tcp_seg;
+  unsigned short int cksum_arr[12];
+  unsigned int i,sum=0, cksum;
+  char * tcp_char_segment;
 
 	//check if user passed port number as argument
 	if (argc < 2) {
@@ -52,8 +58,62 @@ int main(int argc, char **argv)
 	if (connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
 		printf("ERROR while attempting to  connect");
 		exit(1);
-	}
+	}  
 
+  //get source port
+  local_addr_len = sizeof(local_addr);
+  getsockname(sockfd, (struct sockaddr*)&local_addr, &local_addr_len);
+  printf("%d\n", local_addr.sin_port);
+
+  //calculate checksum
+  tcp_seg.src = local_addr.sin_port;
+  tcp_seg.des = portno;
+  tcp_seg.seq = 1;
+  tcp_seg.ack = 2;
+  tcp_seg.hdr_flags = 0x2333;
+  tcp_seg.rec = 0;
+  tcp_seg.cksum = 0;
+  tcp_seg.ptr = 0;
+  tcp_seg.opt = 0;
+
+  memcpy(cksum_arr, &tcp_seg, 24); //Copying 24 bytes
+  
+  printf("0x%04X\n", tcp_seg.src); // Printing all values
+  printf("0x%04X\n", tcp_seg.des);
+  printf("0x%08X\n", tcp_seg.seq);
+  printf("0x%08X\n", tcp_seg.ack);
+  printf("0x%04X\n", tcp_seg.hdr_flags);
+  printf("0x%04X\n", tcp_seg.rec);
+  printf("0x%04X\n", tcp_seg.cksum);
+  printf("0x%04X\n", tcp_seg.ptr);
+  printf("0x%08X\n", tcp_seg.opt);
+ 
+  for (i=0;i<12;i++)               // Compute sum
+   sum = sum + cksum_arr[i];
+
+  cksum = sum >> 16;              // Fold once
+  sum = sum & 0x0000FFFF; 
+  sum = cksum + sum;
+
+  cksum = sum >> 16;             // Fold once more
+  sum = sum & 0x0000FFFF;
+  cksum = cksum + sum;
+
+  /* XOR the sum for checksum */
+  printf("Checksum Value: 0x%04X\n", (0xFFFF^cksum)); //print result
+  tcp_seg.cksum = (0xFFFF^cksum); //assign result to cksum value in struct
+
+  /* copy the tcp_seg to a char array to send to server*/
+  tcp_char_segment = (char *)malloc(sizeof(tcp_seg)); //dynamically allocate char array
+  memcpy(tcp_char_segment, &tcp_seg, sizeof(tcp_seg)); //copy struct to char array
+
+  /* send char array to server */
+  num_sent = write(sockfd, tcp_char_segment, strlen(tcp_char_segment));
+  if (num_sent < 0)
+  {
+    printf("error writing to socket...\n");
+    exit(1);
+  }
 
 	return 0;
 }
